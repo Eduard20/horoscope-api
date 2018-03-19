7dbaba27865a9fbed032e67636eb9c65d3632615
Eduard-Simonyan-test <esimonyan2014@gmail.com>
2018-03-19 20:23:31 +0400

    changed host

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ Routes.push({
 
 (async () => {
     const server = await new Hapi.Server({
+        host: process.env.HOST || process.env.HOSTNAME,
         port: parseInt(process.env.PORT) || 3000
     });
 
