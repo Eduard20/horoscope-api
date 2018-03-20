89ac87ea3dd387d2e03b49c3ce5f791a138745e7
Eduard <esimonyan2014@gmail.com>
2018-03-20 11:31:19 +0400

    deleted protocol from host prop

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ Routes.push({
             title: 'Test API Documentation',
             version: '0.0.1',
         },
-        host: 'https://endpoints-hapi.herokuapp.com'
+        host: 'endpoints-hapi.herokuapp.com'
     };
 
     await server.register([
