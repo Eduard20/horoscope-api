85f930229bc94f8c2ef6a8919b8458c1f463d1f0
Eduard <esimonyan2014@gmail.com>
2018-03-20 09:55:23 +0400

    added zodiacs

--- index.js
+++ index.js
@@ -56,6 +56,57 @@ const categories = [
     }
 ];
 
+const zodiacNames = [
+    {
+        title: "Aries",
+        range: "March 21 - April 20"
+    },
+    {
+        title: "Taurus",
+        range: "April 21 - May 20"
+    },
+    {
+        title: "Gemini",
+        range: "May 21 - June 21"
+    },
+    {
+        title: "Cancer",
+        range: "June 22 - July 22"
+    },
+    {
+        title: "Leo",
+        range: "July 23 - August 23"
+    },
+    {
+        title: "Virgo",
+        range: "August 24 - September 23"
+    },
+    {
+        title: "Libra",
+        range: "September 24 - October 23"
+    },
+    {
+        title: "Scorpio",
+        range: "October 24 - November 22"
+    },
+    {
+        title: "Sagittarius",
+        range: "November 23 - December 21"
+    },
+    {
+        title: "Capricorn",
+        range: "December 22 - January 20"
+    },
+    {
+        title: "Aquarius",
+        range: "January 21 - February 20"
+    },
+    {
+        title: "Pisces",
+        range: "February 21 - March 20"
+    }
+];
+
 Routes.push({
     method: 'GET',
     path: '/api/zodiac/categories',
@@ -73,6 +124,27 @@ Routes.push({
         },
         description: 'Get Categories',
         notes: 'Returns categories list',
+        tags: ['api'],
+    }
+});
+
+Routes.push({
+    method: 'GET',
+    path: '/api/zodiac/names',
+    config: {
+        handler: (request, h) => {
+            return {
+                meta: {
+                    status: '200'
+                },
+                data: zodiacNames,
+                pagination: {
+
+                }
+            }
+        },
+        description: 'Get Zodiac Names',
+        notes: 'Returns zodiacs list',
         tags: ['api'],
     }
 });
