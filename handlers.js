644853d0a28b4c0c2c290e2e15d2d8c77fe5c0b8
Eduard-Simonyan-test <esimonyan2014@gmail.com>
2018-03-25 21:05:30 +0400

    changed api data

--- handlers.js
+++ handlers.js
@@ -2,6 +2,7 @@
 const categories = require('./data/categories');
 const zodiacs = require('./data/zodiacs');
 const rp = require("request-promise-native");
+const moment = require("moment");
 const parseString = require('xml2js').parseString;
 const HOROSCOPE = {
     daily: {
@@ -74,7 +75,7 @@ const handlers = {
     getZodiacByType: async (request, h) => {
         try {
             const { category, type } = request.params;
-            const { date } = request.query;
+            const date = moment(new Date()).format("DD.MM.YYYY");
             if (!urls.daily[category]) return 'something wrong';
             if (HOROSCOPE.daily[category] && HOROSCOPE.daily[category][date]) {
                 console.log('found daily this date');
@@ -82,7 +83,8 @@ const handlers = {
                     console.log('also found weekly');
                     return sendResponse(
                         HOROSCOPE.daily[category][date][type][0],
-                        HOROSCOPE.weekly[type][0][category]
+                        HOROSCOPE.weekly[type][0][category],
+                        HOROSCOPE.daily[category][date].date[0].$
                     )
                 }
                 const week = await getWeekZodiac(options);
@@ -95,7 +97,8 @@ const handlers = {
                         HOROSCOPE.weekly = {...result.horo};
                         return resolve(sendResponse(
                             HOROSCOPE.daily[category][result.horo.date[0].$.today][type][0],
-                            HOROSCOPE.weekly[type][0][category]
+                            HOROSCOPE.weekly[type][0][category],
+                            HOROSCOPE.daily[category][result.horo.date[0].$.today].date[0].$
                         ))
                     });
                 })
@@ -117,7 +120,8 @@ const handlers = {
                         console.log('only weekly found');
                         return resolve(sendResponse(
                             HOROSCOPE.daily[category][result.horo.date[0].$.today][type][0],
-                            HOROSCOPE.weekly[type][0][category]
+                            HOROSCOPE.weekly[type][0][category],
+                            HOROSCOPE.daily[category][result.horo.date[0].$.today].date[0].$
                         ))
                     }
                     const week = await getWeekZodiac(options);
@@ -129,7 +133,8 @@ const handlers = {
                         HOROSCOPE.weekly = { ...weekResult.horo };
                         return resolve(sendResponse(
                             HOROSCOPE.daily[category][result.horo.date[0].$.today][type][0],
-                            HOROSCOPE.weekly[type][0][category]
+                            HOROSCOPE.weekly[type][0][category],
+                            HOROSCOPE.daily[category][result.horo.date[0].$.today].date[0].$
                         ))
                     });
                 });
@@ -149,14 +154,72 @@ async function getWeekZodiac(opts) {
     return await rp(options);
 }
 
-function sendResponse(daily, week) {
-    const data = [
-        {yesterday: daily.yesterday[0]},
-        {today: daily.today[0]},
-        {tomorrow: daily.tomorrow[0]},
-        {tomorrow02: daily.tomorrow02[0]},
-        {week: week[0]}
+function sendResponse(daily, week, dates) {
+    // console.log(dates);
+    // const data = [
+    //     {yesterday: daily.yesterday[0]},
+    //     {today: daily.today[0]},
+    //     {tomorrow: daily.tomorrow[0]},
+    //     {tomorrow02: daily.tomorrow02[0]},
+    //     {week: week[0]}
+    // ];
+    const numbers =  [
+        {
+            "name" : "friendship",
+            "number" : "6.25/10"
+        },
+        {
+            "name" : "love",
+            "number" : "8.6/10"
+        },
+        {
+            "name" : "business",
+            "number" : "3.5/10"
+        }
     ];
+    const data = [
+        {
+            horoscope: {
+                name: "yesterday",
+                data: daily.yesterday[0]
+            },
+            date: dates.yesterday,
+            numbers
+        },
+        {
+            horoscope: {
+                name: "today",
+                data: daily.today[0]
+            },
+            date: dates.today,
+            numbers
+        },
+        {
+            horoscope: {
+                name: "tomorrow",
+                data: daily.tomorrow[0]
+            },
+            date: dates.tomorrow,
+            numbers
+        },
+        {
+            horoscope: {
+                name: "tomorrow02",
+                data: daily.tomorrow02[0]
+            },
+            date: dates.tomorrow02,
+            numbers
+        },
+        {
+            horoscope: {
+                name: "week",
+                data: week[0]
+            },
+            date: 'to be done',
+            numbers
+        },
+    ];
+
     return {
         meta: {
             status: 200
